997ffe07a160ad813187719eeeab6e0f19e5e7aa
refundvalidator <refundvalidator@gmail.com>
2023-03-20 02:10:07 -0500

    Edit Variables

--- main.py
+++ main.py
@@ -31,11 +31,12 @@ def main():
             break
     if len(amount) > 1:
         res = requests.get(f'{url}/cosmos/staking/v1beta1/pool',timeout=30).json()
-        current_amount = amount[len(amount)-1]
         staked_supply = float(res['pool']['bonded_tokens'])
+        current_amount = amount[len(amount)-1]
+
         average_diff = diff/len(amount)
         inflation = (average_diff*(31536000/timeframe)) #31536000 seconds in a year
-        inflation_percentage = inflation/amount[len(amount)-1]
+        inflation_percentage = inflation/current_amount
         apr = (inflation_percentage/(staked_supply/current_amount))
         apr = apr-(community_tax*apr)
 
