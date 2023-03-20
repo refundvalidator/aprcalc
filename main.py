d0c08a4ad0fed8180f489be044d0570e3d7d90a1
refundvalidator <refundvalidator@gmail.com>
2023-03-20 01:45:23 -0500

    Update Variables, Include Community Tax

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ url = "https://rest.unification.io/"
 #url = "http://localhost:1317/"
 
 starttime = time.time()
-timeframe = 6
-store = 100000
 amount = []
+
+timeframe = 6
+store = 14400
+community_tax = 0.02
 
 def start():
     while True:
@@ -19,6 +21,7 @@ def start():
         amount.append(float(res['amount']['amount'])) 
         main()
         time.sleep(timeframe)
+
 def main():
     diff = 0
     for i, a in enumerate(amount):
@@ -28,14 +31,17 @@ def main():
             break
     if len(amount) > 1:
         res = requests.get(f'{url}/cosmos/staking/v1beta1/pool',timeout=30).json()
+        current_amount = amount[len(amount)-1]
         staked_supply = float(res['pool']['bonded_tokens'])
-        average = diff/len(amount)
-        #31536000 seconds in a day
-        inflation = (average*(31536000 / timeframe))
+        average_diff = diff/len(amount)
+        inflation = (average_diff*(31536000/timeframe)) #31536000 seconds in a day
         inflation_percentage = inflation/amount[len(amount)-1]
-        apr = inflation_percentage/((staked_supply/1000000000)/(amount[len(amount)-1]/1000000000))
+        apr = (inflation_percentage/(staked_supply/current_amount))
+        apr = apr-(community_tax*apr)
+
         print(f'Store number {len(amount)} every {timeframe} seconds')
-        print(f'Calculated yearly inflation {round(inflation/1000000000,3):,} FUND')
-        print(f'Calculated inflation percentage {round(inflation_percentage*100,3)}%')
-        print(f'Calculated APR percentage {round(apr*100,3)}%\n')
+        print(f'Current Supply: {round(current_amount/1000000000,3)} FUND')
+        print(f'Calculated yearly inflation: {round(inflation/1000000000,3):,} FUND')
+        print(f'Calculated inflation percentage: {round(inflation_percentage*100,3)}%')
+        print(f'Calculated APR percentage: {round(apr*100,3)}%\n')
 start()
